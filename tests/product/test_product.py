8118df2cb83e12979bd35aee337994de5090437e
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-04-04 21:33:14 -0300

    requisito 1 realizado

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        '1',
+        'Software de Gestão',
+        'Software House',
+        '2022/04/21',
+        '2023/05/21',
+        'MG199948',
+        'instrução 2'
+    )
+
+    assert product.id == '1'
+    assert product.nome_do_produto == 'Software de Gestão'
+    assert product.nome_da_empresa == 'Software House'
+    assert product.data_de_fabricacao == '2022/04/21'
+    assert product.data_de_validade == '2023/05/21'
+    assert product.numero_de_serie == 'MG199948'
+    assert product.instrucoes_de_armazenamento == 'instrução 2'
