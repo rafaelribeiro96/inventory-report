2de5474a7f70c208e0ddcabdc6e8a75085e0affe
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-04-04 21:56:14 -0300

    requisito 2 realizado

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,23 @@
+from collections import Counter
+from datetime import date
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(products):
+        dates_of_manufacture = sorted(
+            [product["data_de_fabricacao"] for product in products]
+        )
+
+        expiration_dates = sorted(
+            [product["data_de_validade"] for product in products if
+             product["data_de_validade"] > str(date.today())])
+
+        companies = [product["nome_da_empresa"] for product in products]
+        most_common_company = Counter(companies).most_common(1)[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {dates_of_manufacture[0]}\n"
+            f"Data de validade mais próxima: {expiration_dates[0]}\n"
+            f"Empresa com mais produtos: {most_common_company}"
+        )
