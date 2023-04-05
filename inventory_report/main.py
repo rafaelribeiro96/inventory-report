8f282bc1406cf3e483fe876d61657305df1fd39b
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-04-04 23:31:03 -0300

    requisito 11

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,11 @@
+import sys
+from inventory_report.inventory.inventory import Inventory
+
+
 def main():
-    pass
+    try:
+        path, type = sys.argv[1:]
+        report = Inventory.import_data(path, type)
+        sys.stdout.write(report)
+    except (ValueError):
+        sys.stderr.write("Verifique os argumentos\n")
